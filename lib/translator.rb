251acb6d6f4910209a21199483dcd2ef27370e67
Payton Gray <paytongray18@gmail.com>
2020-08-19 01:30:43 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -27,8 +27,9 @@ def get_english_meaning(library, jap_emote)
   library = load_library(library)
   library.each do |value, meaning|
     if library[value][:japanese] == jap_emote
-      return value
+      return libray[value]
     end
+    binding.pry
   end
   "Sorry, that emoticon was not found"
 end
